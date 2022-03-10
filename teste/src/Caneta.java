c72c8cbc0660666bd5e67b70010a2277eb6995fa
RatonesR <lfrodrigues703@gmail.com>
2022-03-10 12:59:59 -0300

    aula 04

---
+++ teste/src/Caneta.java
@@ -0,0 +1,32 @@
+public class Caneta{
+    String modelo;
+    String cor;
+    double ponta;
+    int carga;
+    final int CARGA_MAXIMA = 100;
+
+    void escrever(String texto){
+        for (int i = 0; i < texto.length(); i++){
+            if(carga > 0){
+                System.out.print(texto.charAt(i));
+                carga -= 1;
+            } else{
+                System.out.println("CANETA SEM CARGA!");
+            }
+        }
+    }
+
+    void iniciarCaneta(String modelo, String cor, double ponta){
+        this.modelo = modelo;
+        this.cor = cor;
+        this.ponta = ponta;
+        this.carga = CARGA_MAXIMA;
+    }
+
+    String pegarDados(){
+        return "Modelo:" + modelo +
+        "- Cor:" + cor +
+        "- Ponta:" + ponta +
+        "- Carga:" + carga;
+    }
+}
