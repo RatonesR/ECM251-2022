45e151ff728f2399f9f10b4207608c2c0253a6e2
RatonesR <lfrodrigues703@gmail.com>
2022-03-31 12:50:11 -0300

    Simulado 1 - muito a melhorar

---
+++ Simulado 1/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Atividade1 atividade1 = new Atividade1();
+        atividade1.run();
+    }
+}
---
+++ Simulado 1/src/Atividade1.java
@@ -0,0 +1,9 @@
+public class Atividade1 {
+    public void run(){
+        Cliente cliente = new Cliente ();
+        Conta conta = new Conta();
+        System.out.println(conta);
+        Transacoes transacoes = new transacoes();
+
+    }
+}
