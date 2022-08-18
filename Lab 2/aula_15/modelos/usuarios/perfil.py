7cabd874a0e698751987c6f0c79cbfbbd9db8109
LUIZ FERNANDO RODRIGUES <19.01358-2@maua.br>
2022-08-18 12:31:16 -0300

    adicionando perfil a classe usuario

--- Lab 2/aula_15/modelos/usuarios/perfil.py
+++ Lab 2/aula_15/modelos/usuarios/perfil.py
@@ -1,9 +1,19 @@
 from datetime import date
 
 class Perfil:
-    def __init__(self, nome, foto=""):
+    def __init__(self, nome):
         self._nome = nome
-        self._foto = foto
+        self._foto = None
         self._data_nascimento = date.today()
         self._descricao = "Sem descricao no momento"
         self._telefone = ""
+    def set_nome(self, nome):
+        self._nome = nome
+    def set_foto(self, foto):
+        self._foto = foto
+    def set_data_nascimento(self, data):
+        self._data = data
+    def set_descricao(self, descricao):
+        self._descricao = descricao
+    def set_telefone(self, telefone):
+        self._telefone = telefone
