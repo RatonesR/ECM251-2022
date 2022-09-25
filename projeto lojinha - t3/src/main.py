f2403b2f08fc3971b309928d3c15e7bebfe66987
RatonesR <lfrodrigues703@gmail.com>
2022-09-24 22:51:52 -0300

    salvando pra dormir

--- projeto lojinha - t3/src/main.py
+++ projeto lojinha - t3/src/main.py
@@ -3,6 +3,13 @@ import streamlit as st
 st.set_page_config(page_title="Rat Store", page_icon=":mouse:", layout="centered", initial_sidebar_state="collapsed", menu_items=None)
 
 
+with st.sidebar:
+    st.sidebar.button("LOGIN")
+    st.sidebar.button("LOJA")
+    st.sidebar.button("CARRINHO")
+    st.sidebar.button("PERFIL")
+    st.sidebar.button("SAIR")
+
 col1, col2, col3 = st.columns(3)
 with col2:
     st.title("**WELCOME TRAVELER**")
@@ -15,13 +22,13 @@ st.text_input("Password", "")
 
 
 col1, col2, col3, col4, col5 = st.columns(5)
+
+def logar():
+    st.write("login succesful!")
+
 with col1:
-    login = st.button("Login")
+    login = st.button(label="Login", on_click=logar)
     st.write(login)
-    if login:
-        st.caption("login successful")
-    else:
-        st.caption("wrong username or password")
 
 with col2:
     cadastrar = st.button("Cadastrar")
