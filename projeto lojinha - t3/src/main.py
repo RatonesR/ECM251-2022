38f9d8ed976186477a035d4f4c0779166f323a5e
RatonesR <lfrodrigues703@gmail.com>
2022-09-26 15:15:41 -0300

    sidebar de pagamento

--- projeto lojinha - t3/src/main.py
+++ projeto lojinha - t3/src/main.py
@@ -2,35 +2,37 @@ import streamlit as st
 
 st.set_page_config(page_title="Rat Store", page_icon=":mouse:", layout="centered", initial_sidebar_state="collapsed", menu_items=None)
 
+login_cadastro, loja, carrinho, perfil = st.tabs(["login e cadastro", "loja", "carrinho", "perfil"])
+
+payment = ["PayPal", "Boleto Bancario", "PIX", "Visa", "Mastercard", "Diner's Club"]
 
 with st.sidebar:
-    st.sidebar.button("LOGIN")
-    st.sidebar.button("LOJA")
-    st.sidebar.button("CARRINHO")
-    st.sidebar.button("PERFIL")
-    st.sidebar.button("SAIR")
+        st.sidebar.title("CARRINHO")
+        with st.expander("itens"):
+            st.write("seus itens")
+        st.write("total da compra:")
+        option = st.selectbox(label="forma de pagamento:", options=payment)
+        st.button(label="pagar")
 
-col1, col2, col3 = st.columns(3)
-with col2:
-    st.title("**WELCOME TRAVELER**")
-
-
-st.markdown("**LOGIN**")
-
-st.text_input("Username", "")
-st.text_input("Password", "")
+with login_cadastro:
+    col1, col2, col3 = st.columns(3)
+    with col2:
+        st.title("**WELCOME TRAVELER**")
 
 
-col1, col2, col3, col4, col5 = st.columns(5)
+    st.markdown("**LOGIN**")
 
-def logar():
-    st.write("login succesful!")
+    st.text_input("Username")
+    st.text_input("Password")
 
-with col1:
-    login = st.button(label="Login", on_click=logar)
-    st.write(login)
 
-with col2:
-    cadastrar = st.button("Cadastrar")
-    st.write(cadastrar)
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        if st.button(label="Login"):
+            st.write("login succesful!")
+
+    with col2:
+        if st.button("Cadastrar"):
+            st.write("signed in!")
 
