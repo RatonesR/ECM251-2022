59a3b19738ee766a5f9806d6751fdd51d5391559
LUIZ FERNANDO RODRIGUES <19.01358-2@maua.br>
2022-10-06 12:54:28 -0300

    trabalho t3 pronto - com descricao dos produtos

--- projeto lojinha - t3/src/main.py
+++ projeto lojinha - t3/src/main.py
@@ -1,9 +1,8 @@
 import streamlit as st
-from controllers.user_controller import UserController as uc
 
 st.set_page_config(page_title="Rat Store", page_icon=":mouse:", layout="centered", initial_sidebar_state="collapsed", menu_items=None)
 
-login, cadastro, loja, carrinho = st.tabs(["login", "cadastro", "loja", "carrinho"])
+login, cadastro, loja, carrinho, teste = st.tabs(["login", "cadastro", "loja", "carrinho", "teste"])
 
 payment = ["PayPal", "Boleto Bancario", "PIX", "Visa", "Mastercard", "Diner's Club"]
 
@@ -104,14 +103,30 @@ with loja:
         st.markdown("Aulão com Murilo")
         st.image("https://avatars.githubusercontent.com/u/24915894?v=4")
         st.info("R$ 29,99")
+        with st.expander("descrição"):
+            st.write("""
+            Uma aula particular com o ilustre Murilo Zanini sem julgamentos, 
+            a não ser que seja para aprender a commitar...
+            """)
     with col2:
         st.markdown("Correção de Trabalho")
         st.image("https://docplayer.com.br/docs-images/79/79604769/images/16-0.jpg")
         st.info("R$ 9999,99")
+        with st.expander("descrição"):
+            st.write("""
+            Corrija seu trabalho que está há mais de um semestre parado,
+            com um prazo de até 99 dias úteis!
+            """)
     with col3:
         st.markdown("Pokemon Scarlet")
         st.image("https://styles.redditmedia.com/t5_5x8srd/styles/communityIcon_ltry6sr9afk81.png?width=256&s=34d711da385cf952b463e85ddc5d88d5f9249602")
         st.info("R$ 299,99")
+        with st.expander("descrição"):
+            st.write("""
+            Simplesmente o melhor pokemon que você já jogou! 
+            Explore um mundo incrível cheio de aventura e pokemons com ou sem a ajuda de seus amigos! 
+            O Scarlet é o melhor mesmo.
+            """)
 
     st.header("**PROMOÇÕES**")
 
@@ -120,14 +135,29 @@ with loja:
         st.markdown("Adiar o Projeto")
         st.image("https://images.vexels.com/media/users/3/212007/isolated/lists/307b1160d0d080207771fdde7d0c0724-ampulheta-do-magico.png")
         st.info("R$ 199,99")
+        with st.expander("descrição"):
+            st.write("""
+            Adie aquele seu projeto que está na data limite de entrega! 
+            Extensão de prazo de uma hora.
+            """)
     with col2:
         st.markdown("Bread Simulator")
         st.image("https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/bread_1f35e.png")
         st.info("R$ 4,99")
+        with st.expander("descrição"):
+            st.write("""
+            Já imaginou como é a vida de um pão de forma? 
+            Agora você pode viver como se fosse um!
+            """)
     with col3:
         st.markdown("PC gamer")
         st.image("https://img.olx.com.br/thumbs256x256/77/776270067619013.jpg")
         st.info("R$ 4499,99")
+        with st.expander("descrição"):
+            st.write("""
+            Um PC gamer para rodar seu Bread Simulator, 
+            porque não, não é um jogo leve como você imaginou!
+            """)
 
 with carrinho:
     st.title("**SEUS ITENS**")
@@ -154,3 +184,9 @@ with carrinho:
             removed_element = st.session_state.carrinho_preco.pop(-1)
             st.info("Recarregue a página para atualizar")
 
+with teste:
+    with st.expander("descrição"):
+        st.write("""
+        Uma aula particular com o ilustre Murilo Zanini sem julgamentos, 
+        a não ser que seja para aprender a commitar...
+        """)
