803f022a8a60fc9d643f4def43a7a5079e437484
RatonesR <lfrodrigues703@gmail.com>
2022-11-10 14:44:45 -0300

    cadastro e login funcionando no streamlit, comecando loja

--- Projeto-T4/src/sistema.py
+++ Projeto-T4/src/sistema.py
@@ -9,7 +9,22 @@ estado_loja = EstadoLoja()
 
 st.set_page_config(page_title="Login", page_icon=":mouse:", layout="centered", initial_sidebar_state="collapsed", menu_items=None)
 
-while estado_loja.estado() == 0:
+loja = estado_loja.estado()
+
+if loja == 0:
     login.tela_login()
-while estado_loja.estado() == 1:
+if loja == 1:
     cadastro.tela_cadastro()
+
+
+
+# st.sidebar.title("MENU")
+# with st.sidebar:
+#     menu = st.selectbox('O que deseja acessar?', options=['login', 'cadastro', 'meu perfil', 'produtos', 'promoções', 'carrinho'])
+
+
+# st.title("**BEM VINDO À RAT STORE! 🐀**")
+# st.subheader("**A MELHOR LOJA ONLINE DO BRASIL!**")
+
+# if menu == 'login':
+#     login.tela_login()
--- Projeto-T4/tests/teste_user_dao.py
+++
@@ -1,4 +0,0 @@
-from src.dao.user_dao import UserDAO
-from src.dao.cart_dao import CartDAO
-from src.models.user import User
-
